a5ef1eeb6d478856193dbeb71c15357e13f85b65
user.name <gideonad4@gmail.com>
2024-07-14 10:10:03 +0100

    learning debug

--- lab_3_wu1.m
+++ lab_3_wu1.m
@@ -6,5 +6,5 @@ x.timeInterval = 0:(1/x.fs):0.05;
 x.values = x.Amp*cos(2*pi*(x.freq)*(x.timeInterval) + x.phase);
 x.name = 'My Signal';
 x
-plot(x.timeInterval, x.values);
+plot(x.timeInterval, x.values)
 title(x.name)
