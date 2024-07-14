da344a3f15fb9d7dfda53898e27eb07aa9596cfd
k-ojo <gideonad4@gmail.com>
2024-07-14 11:46:18 -0700

    added files

--- lab_3/play_scale.m
+++ lab_3/play_scale.m
@@ -12,12 +12,19 @@ n1 = 1;
 for kk = 1:length(scale.keys)
     keynum = scale.keys(kk);
     
-    tone = key2note(10, keynum, scale.durations(kk));
+    tone = key2note(100, keynum, scale.durations(kk));
     
     n2 = n1 + length(tone) - 1;
     xx(n1:n2) = xx(n1:n2) + tone;
     
     n1 = n2 + 1;
 end
+
+figure;
+plotspec(xx, 11025, 2048);
+% specgram(xx, 2048, 11025);
+grid on;
+colormap(1-gray(256));
+title('play scale');
 
 soundsc(xx, fs)
