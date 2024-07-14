5dc207be5f109ad5f6575debaf2b0540c18814a2
k-ojo <gideonad4@gmail.com>
2024-07-14 04:57:53 -0700

    modified coscos

--- lab_3/coscos.m
+++ lab_3/coscos.m
@@ -5,7 +5,7 @@ t1 = 0:(1/fs):dur;
 t2 = 0:(1/f2):dur;
 cos1 = cos(2*pi*f1*t1);
 cos2 = cos(2*pi*f2*t2);
-
+dbstop if error
 xx = cos1.*cos2;
 tt = t1;
 end
---
+++ lab_3/lab_3_wu_debug.m
@@ -0,0 +1,3 @@
+%% type this at the command line prompt fx>> 
+%% or run this file
+coscos(2,s3,20,1);
