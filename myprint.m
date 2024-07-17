31251b29d9e66188b5fa836ac936e3ff3746b707
k-ojo <gideonad4@gmail.com>
2024-07-17 08:02:06 -0700

    added files

--- myprint.m
+++ myprint.m
@@ -1,7 +1,7 @@
-function myprint(name, mytitle, label_y, label_x)
+function myprint(name, mytitle, label_y, label_x, lab)
     title(mytitle);
     ylabel(label_y);
     xlabel(label_x);
-    file = strcat('C:\Users\Freedom Academy\Documents\LAB\matlab_labs\prints\', name);
+    file = strcat('C:\Users\Freedom Academy\Documents\LAB\matlab_labs\prints\', name, lab);
     print(file, '-dpng');
 end
