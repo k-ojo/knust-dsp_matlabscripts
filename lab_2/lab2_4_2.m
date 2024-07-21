d6987cdcdc2ddd4f018effcc6b3df7873b27593b
k-ojo <gideonad4@gmail.com>
2024-07-21 09:57:27 -0700

    added lab 3

--- lab_2/lab2_4_2.m
+++ lab_2/lab2_4_2.m
@@ -1,11 +1,13 @@
 [xx, tt] = beat(1, 1, 2000, 32, 11025, 0.26);
 
+fsamp = 11025;
 figure;
 plot(tt, xx);
+xlim([0, 0.26]);
 title('Beat Signal');
 xlabel('Time (s)');
 ylabel('Amplitude');
-xlim([0, 0.26]);
+
 print('C:\Users\Freedom Academy\Documents\LAB\lab4.4.0_plot', '-dpng');
 
 figure;
--- lab_3_b/lab_3_b_1.m
+++ lab_3_b/lab_3_b_1.m
@@ -1,5 +1,8 @@
 colormap(gray(256));
 load zone.mat;
 
-xpix = ones(256, 1)*cos(2*pi*(0:255)/16);
-show_img(xpix, 0, 1, gray(256));
+%% since 400/80 = 5 
+xpix = ones(400, 1)*cos(2*pi*(0:399)/80);
+%%use transpose to find the horizontal
+show_img(xpix.', 0 ,1, gray(256));
+%%myprint('Name', 'title', 'hight', 'length', '3');
