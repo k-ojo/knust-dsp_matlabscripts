f0e7e390f589652bf276a3e7636f7b9e4d94970b
k-ojo <gideonad4@gmail.com>
2024-07-21 13:42:27 -0700

    added .mat file

--- lab_3_b/lab_3_b_1.m
+++ lab_3_b/lab_3_b_1.m
@@ -1,8 +1,13 @@
 colormap(gray(256));
-load zone.mat;
-
+load lighthouse.mat
 %% since 400/80 = 5 
 xpix = ones(400, 1)*cos(2*pi*(0:399)/80);
 %%use transpose to find the horizontal
-show_img(xpix.', 0 ,1, gray(256));
+show_img(xpix.', 1 ,1, gray(256));
+trusize();
 %%myprint('Name', 'title', 'hight', 'length', '3');
+show_img(ww, 2, 1, gray(256));
+trusize();
+p = 2;
+wp = ww(1:p:end,1:p:end);
+show_img(wp, 3, 1, gray(256));
